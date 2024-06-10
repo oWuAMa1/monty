4eca97638269f5fafa195f3cbb045006af185132
owuama1 <kelechiowuama24@gmail.com>
2024-06-10 13:51:14 +0000

    div operation

--- monty.h
+++ monty.h
@@ -356,6 +356,38 @@ void sub(stack_t **stack, unsigned int line_number)
     (*stack)->prev = NULL;
     free(temp);
 }
+/**
+ * div_op - Divides the second top element of the stack by the top element.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the instruction appears.
+ *
+ * Description: The result is stored in the second top element of the stack,
+ * and the top element is removed. If the stack contains less than two elements,
+ * or if the top element is 0, appropriate error messages are printed, and
+ * the program exits with EXIT_FAILURE.
+ */
+void div_op(stack_t **stack, unsigned int line_number)
+{
+    stack_t *temp;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%u: division by zero\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = *stack;
+    (*stack)->next->n /= (*stack)->n;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    free(temp);
+}
 
 /**
  * execute_instruction - Executes an instruction
@@ -373,6 +405,7 @@ void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number
         {"swap", swap},
         {"add", add},
         {"sub", sub},
+				{"div_op", div_op},
         {"nop", nop},
         {NULL, NULL}
     };

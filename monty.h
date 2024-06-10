a207c81ae1b5f5700ccda460f0ccdde4ee3cd20f
owuama1 <kelechiowuama24@gmail.com>
2024-06-10 13:55:58 +0000

    mul function

--- monty.h
+++ monty.h
@@ -388,6 +388,33 @@ void div_op(stack_t **stack, unsigned int line_number)
     (*stack)->prev = NULL;
     free(temp);
 }
+/**
+ * mul - Multiplies the second top element of the stack with the top element.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the instruction appears.
+ *
+ * Description: If the stack contains less than two elements, prints an error
+ * message and exits with status EXIT_FAILURE. The result is stored in the
+ * second top element of the stack, and the top element is removed. At the end:
+ * - The top element of the stack contains the result.
+ * - The stack is one element shorter.
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+    stack_t *temp;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = *stack;
+    (*stack)->next->n *= (*stack)->n;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    free(temp);
+}
 
 /**
  * execute_instruction - Executes an instruction
@@ -406,6 +433,7 @@ void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number
         {"add", add},
         {"sub", sub},
 				{"div", div_op},
+        {"mul", mul},
         {"nop", nop},
         {NULL, NULL}
     };
